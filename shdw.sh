750f215068010ca9c02efb0cf1c8f2d70a42c83f
Jonathan Damico <jj_damico@yahoo.com>
2016-07-14 15:05:38 -0700

    make install occur BEFORE starting up shdw

--- shdw.sh
+++ shdw.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-export SHDWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export SHDWUSER=$1
-
-bash --rcfile $SHDWDIR/rc/bashrc
-
 if [ ! -d "${HOME}/.config/thefuck" ]; then
     sudo apt update
     sudo apt install python3-dev python3-pip
     sudo -H pip3 install thefuck
 fi
 
+export SHDWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SHDWUSER=$1
+
+bash --rcfile $SHDWDIR/rc/bashrc
