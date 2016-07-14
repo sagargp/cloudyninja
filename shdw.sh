5c87118bca2d95757377a70b5686c69024845553
Jonathan Damico <jj_damico@yahoo.com>
2016-07-14 10:46:17 -0700

    install theFuck on startup if not installed

--- shdw.sh
+++ shdw.sh
@@ -4,3 +4,10 @@ export SHDWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SHDWUSER=$1
 
 bash --rcfile $SHDWDIR/rc/bashrc
+
+if [ ! -d "${HOME}/.config/thefuck" ]; then
+    sudo apt update
+    sudo apt install python3-dev python3-pip
+    sudo -H pip3 install thefuck
+fi
+
