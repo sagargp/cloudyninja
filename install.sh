25c58085e9dbcef35c5a2614681167c0b276f84d
Sagar Pandya <sagargp@gmail.com>
2017-04-11 00:14:40 -0600

    Added an install script to make shdw settings permanent on new shells

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export SHDWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SHDWUSER=$1
+export HIDESHDWUSER=1
+export PROFILE_SOURCED=1
+
+mv $HOME/.bashrc $HOME/.bashrc.bak
+mv $HOME/.bash_profile $HOME/.bash_profile.bak
+
+echo "export SHDWDIR=$SHDWDIR" > $HOME/.bash_profile
+echo "export SHDWUSER=$SHDWUSER" >> $HOME/.bash_profile
+echo "export HIDESHDWUSER=1" >> $HOME/.bash_profile
+echo "export PROFILE_SOURCED=1" >> $HOME/.bash_profile
+echo "source $HOME/.bashrc" >> $HOME/.bash_profile
+
+ln -s $SHDWDIR/rc/bashrc $HOME/.bashrc
